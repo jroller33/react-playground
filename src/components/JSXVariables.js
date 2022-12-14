e7f48e0a2e71a3f56539a48d33c6a2bbed96fc13
John Roller <jtroller33@gmail.com>
2022-12-13 23:07:04 -0600

    JSX working but didn't add bootstrap

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -1,21 +1,18 @@
 import React from 'react';
 
-// TODO: Create a `name` variable
-
-// TODO: Create a `thoughts` variable
-// This should be a string regarding what you think of React
-
 function JSXVariables() {
+  const name = "John";
+  const nameLength = name.length;
+  const thoughts = "react is ok";
+
+
   return (
     <div className="main-container">
       <div className="container">
         <div className="jumbotron">
-          {/* TODO: Add your name variable */}
-          <h1>Hi! My name is (insert name here)</h1>
-          {/* TODO: Add your number of letters variable */}
-          <h2>My name has (insert number of letters in name here) letters</h2>
-          {/* TODO: Add your thoughts variable */}
-          <h2>I think React (insert thoughts about React here)</h2>
+          <h1>Hi! My name is {name}</h1>
+          <h2>My name has {nameLength} letters</h2>
+          <h2>I think React {thoughts}</h2>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+
+// this is supposed to use bootstrap
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
