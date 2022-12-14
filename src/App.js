1c2898fdf0b65846737d2aadf0883e8046de6f4d
John Roller <jtroller33@gmail.com>
2022-12-13 22:34:19 -0600

    02 done

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
 import React from 'react';
-import './App.css';
 
-const displayEmojiName = event => alert(event.target.id);
-const emojis = [
-  {
-    emoji: '😀',
-    name: "test grinning face"
-  },
-  {
-    emoji: '🎉',
-    name: "party popper"
-  },
-  {
-    emoji: '💃',
-    name: "woman dancing"
-  }
-];
+// imports a component that we use in App()
+import HelloReact from './components/HelloReact';
 
 function App() {
-  const greeting = "greeting";
-  const displayAction = false;
-  return(
-    <div className="container">
-      <h1 id={greeting}>Hello, World</h1>
-      {displayAction && <p>I am writing JSX</p>}
-      <ul>
-        {
-          emojis.map(emoji => (
-            <li key={emoji.name}>
-              <button
-                onClick={displayEmojiName}
-              >
-                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
-              </button>
-            </li>
-          ))
-        }
-      </ul>
-    </div>
-  )
+  return <HelloReact />;
 }
 
 export default App;
---
+++ src/components/HelloReact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function HelloReact() {
+  const text = 'some text';
+
+  // this is JSX
+  return <p>Hello World! Here is {text}</p>;
+}
+
+export default HelloReact;
