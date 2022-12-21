09470ed300806074c3b66cb322aa6d690e74a554
John Roller <jtroller33@gmail.com>
2022-12-21 12:59:56 -0600

    06 is finished

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 
-// this is supposed to use bootstrap
+// to use bootstrap with react:
+// paste this line in index.js
 import 'bootstrap/dist/css/bootstrap.min.css';
+// add these to package.json:
+// "axios": "^0.27.2",
+// "bootstrap": "^5.2.2",
+// "dotenv": "^16.0.3",
+// use classnames with HTML
+
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
