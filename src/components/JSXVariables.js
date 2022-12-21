16c716623f1e170737555572b4830e2bdee914fb
John Roller <jtroller33@gmail.com>
2022-12-20 18:42:17 -0600

    components

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -13,7 +13,7 @@ function JSXVariables() {
         <div className="jumbotron">
           <h1>Hi! My name is {name}</h1>
           <h2>My name has {name.length} letters</h2>
-          <h2>I think React {thoughts}</h2>
+          <h2>I think {thoughts}</h2>
         </div>
       </div>
     </div>
