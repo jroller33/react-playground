7baaaca419a3ac7dcd0781657d08e57d6de3cd1c
John Roller <jtroller33@gmail.com>
2022-12-21 13:46:45 -0600

    list component

--- src/components/List.js
+++ src/components/List.js
@@ -3,12 +3,12 @@ import React from 'react';
 function List(props) {
   return (
     <ul className="list-group">
-      {/* {} */}
       {props.groceries.map(item => {
         return (
           <li className='list-group-item' key={item.id}>{item.name}</li>
-        );
-      })}
+          );
+        })}
+        {/* {everything needs a unique key for react to be able to update it} */}
     </ul>
   );
 }
