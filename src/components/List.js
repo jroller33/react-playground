a55984d88e34d826333827d0c58b78a79eb640ad
John Roller <jtroller33@gmail.com>
2022-12-21 13:44:03 -0600

    list component done

--- src/components/List.js
+++ src/components/List.js
@@ -3,10 +3,10 @@ import React from 'react';
 function List(props) {
   return (
     <ul className="list-group">
-      {/* TODO: Use the `map` method to iterate over each grocery and return a `li` element that contains the item's name and has a unique key attribute */}
-      {props.groceries.map((item) => {
+      {/* {} */}
+      {props.groceries.map(item => {
         return (
-          // TODO: Your code here
+          <li className='list-group-item' key={item.id}>{item.name}</li>
         );
       })}
     </ul>
